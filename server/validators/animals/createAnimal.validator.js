b60d7860c1925e2389e099016c6e76a6f2057465
kyle <nojaim.k@gmail.com>
2026-03-02 19:36:38 -0500

    added strict to zod object body parameters

--- server/validators/animals/createAnimal.validator.js
+++ server/validators/animals/createAnimal.validator.js
@@ -1,21 +1,23 @@
 const { z } = require('zod');
 
 const createAnimalSchema = z.object({
-  body: z.object({
-    name: z.string(),
-    chip_id: z.int(),
-    dob: z.coerce.date(),
-    sex: z.enum(['MALE', 'FEMALE']),
-    species: z.enum(['CAT', 'DOG']),
-    foster_status: z.enum(['SHELTERED', 'FOSTERED', 'ADOPTED']),
-    kennel_id: z.int(),
-    altered: z.boolean(),
-    weight: z.number().nonnegative(),
-    last_modified: z.coerce.date(),
-    // should this be z.url() ?
-    picture: z.string(),
-    modified_by: z.uuid(),
-  }),
+  body: z
+    .object({
+      name: z.string(),
+      chip_id: z.int(),
+      dob: z.coerce.date(),
+      sex: z.enum(['MALE', 'FEMALE']),
+      species: z.enum(['CAT', 'DOG']),
+      foster_status: z.enum(['SHELTERED', 'FOSTERED', 'ADOPTED']),
+      kennel_id: z.int(),
+      altered: z.boolean(),
+      weight: z.number().nonnegative(),
+      last_modified: z.coerce.date(),
+      // should this be z.url() ?
+      picture: z.string(),
+      modified_by: z.uuid(),
+    })
+    .strict(),
   params: z.object({}).optional(),
   query: z.object({}).optional(),
 });
