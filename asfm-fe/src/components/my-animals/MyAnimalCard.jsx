2454bdfeb732f9801310b6c01f8b1a4bdab38b95
Devan Rivera <devanrivera98@gmail.com>
2026-03-04 19:10:28 -0800

    mapped out my animal cards based on the user animals

--- asfm-fe/src/components/my-animals/MyAnimalCard.jsx
+++ asfm-fe/src/components/my-animals/MyAnimalCard.jsx
@@ -1,23 +1,29 @@
+import getBirthdayYear from '@/utils/getBirthday';
 import { Card } from '../ui/card';
 
-export default function MyAnimalCard() {
+export default function MyAnimalCard({animal}) {
+
+    const age = getBirthdayYear(animal.dob)
+
   return (
     <>
       <Card className="flex flex-row items-center gap-4 p-4">
         <img
-          src="https://www.guidingeyes.org/wp-content/uploads/2024/08/Slater.jpeg"
+          src={animal.picture}
+          alt={`adoption picture of ${animal.species} ${animal.name}`}
           className="h-16 w-16 rounded-xl object-fit flex-shrink-0"
         />
         <div className="flex flex-wrap gap-5 justify-between w-full">
           <div className="flex flex-wrap gap-5">
-            <AnimalStatPill label={'animal id'} data={'A-20481'} />
-            <AnimalStatPill label={'name'} data={'Billy'} />
-            <AnimalStatPill label={'age'} data={'3'} />
-            <AnimalStatPill label={'Sex'} data={'male'} />
-            <AnimalStatPill label={'Fixed Status'} data={'male'} />
+            <AnimalStatPill label={'animal id'} data={animal.chip_id ?? 'No id'} />
+            <AnimalStatPill label={'name'} data={animal.name ?? 'No name'} />
+            <AnimalStatPill label={'age'} data={age ? `${age} ${age > 1 ? 'yrs': 'yr'}` : 'No age'}/>
+            <AnimalStatPill label={'Species'} data={animal.species ?? 'Unknown'} />
+            <AnimalStatPill label={'Sex'} data={animal.sex ?? 'Sex unknown'} />
+            <AnimalStatPill label={'Fixed Status'} data={animal.altered ? 'Fixed' : 'Not Fixed'} />
           </div>
           <div className="flex justify-center w-full md:w-auto">
-            <a className="ring-1 ring-gray-200 rounded-xl bg-gray-50 px-4 py-2 flex flex-col justify-center gap-y-0.5 w-full md:w-auto items-center">
+            <a href={`/single-animal/${animal.id}`} className="ring-1 ring-gray-200 rounded-xl bg-gray-50 hover:bg-gray-100 px-4 py-2 flex flex-col justify-center gap-y-0.5 w-full md:w-auto items-center hover:underline underline-offset-2">
               Read More
             </a>
           </div>
--- asfm-fe/src/pages/MyAnimalsListPage.jsx
+++ asfm-fe/src/pages/MyAnimalsListPage.jsx
@@ -1,6 +1,48 @@
 import MyAnimalCard from "@/components/my-animals/MyAnimalCard"
+import { useEffect, useState } from "react";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function MyAnimalsListPage() {
+
+    const [myAnimals, setMyAnimals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false)
+
+    const url = 'http://localhost:3005'  // <-- placeholder
+
+    async function fetchMyAnimals(userId) {
+        const response = await fetch(`${url}/animals?user_id=${userId}`)
+        if (!response.ok) {
+            setIsError(true);
+            throw new Error('Failed to fetch my animals', response.status)
+        } else {
+            const data = await response.json();
+            console.log(data)
+            setMyAnimals(data)
+        }
+    }
+
+    useEffect(() => {
+        fetchMyAnimals("550e8400-e29b-41d4-a716-446655440202")
+        setIsLoading(false)
+    }, [])
+
+    if (isLoading)
+    return (
+      <div className="flex justify-center pt-10">
+        <Spinner className="size-12 text-primary" />
+      </div>
+    );
+
+    if (isError) {
+        return (
+          <div className="flex flex-col items-center justify-center h-screen text-center px-4">
+            <h2 className="text-2xl font-semibold mb-4">Oops!</h2>
+            <p className="text-lg text-gray-700">That animal doesn't seem to be available right now.</p>
+            <p className="mt-2 text-gray-500">Please try searching again or check back later.</p>
+          </div>
+        );
+      }
 
     return (
         <div className="max-w-7xl mx-auto py-20 px-4 sm:px-6 lg:px-8">
@@ -8,8 +50,20 @@ export default function MyAnimalsListPage() {
                 <h1 className="text-center text-2xl">My Animals</h1>
             </div>
             <div className="mt-10 p-4 ring-2 rounded-lg flex flex-col gap-y-5 min-h-screen bg-accent">
-                <MyAnimalCard />
-                <MyAnimalCard />
+                {myAnimals.length === 0 ? 
+                <div className="flex flex-col  items-center pt-5">
+                    <h2 className="text-2xl font-semibold">No animals registered</h2>
+                    <p className="text-lg pt-5">
+                        Animals you register will show up here.
+                    </p>
+                </div>
+                :    
+                <>
+                {myAnimals.map((animal, index) => (
+                    <MyAnimalCard key={index} animal={animal} />
+                ))}
+                </> 
+            }
             </div>
         </div>
 
