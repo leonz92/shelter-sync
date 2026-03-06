a6ea52324a256dcd45afafc674e5caed0494da91
kyle <nojaim.k@gmail.com>
2026-03-05 22:13:10 -0500

    set up routes, use controller to check for real-world logic flaws

--- server/features/inventory-transaction/inventory-transaction.routes.js
+++ server/features/inventory-transaction/inventory-transaction.routes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const inventoryTransactionController = require('./inventory-transaction.controller');
+const staffAuthCheck = require('../../middleware/auth-staff');
+const validate = require('../../middleware/validator');
+const intakeTransactionSchema = require('../../validators/inventory-transaction/intakeTransaction.validator');
+const distributeTransactionSchema = require('../../validators/inventory-transaction/distributeTransaction.validator');
 
 router.get('/', inventoryTransactionController.getAllInventoryTransactions);
 router.get('/:id', inventoryTransactionController.getInventoryTransactionById);
-router.post('/', inventoryTransactionController.createNewInventoryTransaction);
+router.post('/intake', staffAuthCheck, validate(intakeTransactionSchema), inventoryTransactionController.createIntakeTransaction);
+router.post('/distribute', staffAuthCheck, validate(distributeTransactionSchema), inventoryTransactionController.createDistributeTransaction);
 
 module.exports = router;
