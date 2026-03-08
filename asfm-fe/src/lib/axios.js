468e6ccbffa50977af1d45987f115c779eb93dcb
kyle <nojaim.k@gmail.com>
2026-03-08 19:32:44 -0400

    connected us to the real server

--- asfm-fe/src/lib/axios.js
+++ asfm-fe/src/lib/axios.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   // This points to real server API
-  // baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://localhost:8080/api',
 
   // This points to mock API
-  baseURL: 'http://localhost:3001/api',
+  // baseURL: 'http://localhost:3001/api',
 
   timeout: 15000,
   headers: {
