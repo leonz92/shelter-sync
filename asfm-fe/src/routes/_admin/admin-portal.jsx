271ba9d2abece819a7c082b14e303d10a480490c
kyle <nojaim.k@gmail.com>
2026-03-08 19:32:07 -0400

    moved admin portal from _protected to _admin

--- asfm-fe/src/routes/_admin/admin-portal.jsx
+++ asfm-fe/src/routes/_admin/admin-portal.jsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
 import AdminPortal from '@/admin-portal';
 
-export const Route = createFileRoute('/_protected/admin-portal')({
+export const Route = createFileRoute('/_admin/admin-portal')({
   component: RouteComponent,
 });
 
