65dbfa6f843ec76f125c4b62c905f2e013225cc6
dylanyng <dylanyoung@hey.com>
2026-03-08 16:53:26 -0400

    wire up the post route

--- server/features/medical-logs/medical-logs.routes.js
+++ server/features/medical-logs/medical-logs.routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const authUserCheck = require('../../middleware/auth-user');
 const validate = require('../../middleware/validator');
 const getAllMedicalLogsSchema = require('../../validators/medical-logs/getMedicalLogs.validator');
+const createMedicalLogSchema = require('../../validators/medical-logs/createMedicalLog.validator');
 const medicalLogController = require('./medical-logs.controller');
 
 router.get('/', authUserCheck, validate(getAllMedicalLogsSchema), medicalLogController.getAllMedicalLogs);
 router.get('/:id', medicalLogController.getMedicalLogById);
+router.post('/', authUserCheck, validate(createMedicalLogSchema), medicalLogController.createMedicalLog);
 
 module.exports = router;
